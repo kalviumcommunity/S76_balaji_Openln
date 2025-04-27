1bc7c1fd7fe934e2e66a0cb507c74206c6b03bd9
balaji-r-2007 <balaji648balaji@gmail.com>
2025-04-27 14:00:33 +0530

    created server and get api

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.use(cors({
+  origin: "http://localhost:5173", // Frontend URL
+  credentials: true,
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
