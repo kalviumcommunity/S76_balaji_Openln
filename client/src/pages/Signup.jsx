246d71d447f7825b0b95e6b884e253f0399a27e2
balaji-r-2007 <balaji648balaji@gmail.com>
2025-06-27 10:30:40 +0530

    fix1

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -30,7 +30,7 @@ const Signup = () => {
       setLoading(true);
       setError("");
       
-      const response = await fetch("http://localhost:5000/api/auth/signup", {
+      const response = await fetch("https://s76-balaji-openln.onrender.com/api/auth/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
